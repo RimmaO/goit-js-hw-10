fb8ca8395167a977fb0a826ffc6e21c23096f0a4
RimmaO <rimma_o@ukr.net>
2023-05-17 16:24:27 +0300

    fixed task

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ function onInput(event) {
 }
 
 function fetchCountries(name = '') {
-  const BASE_URL = 'https://restcountries.com/v3.1/name';
+  const BASE_URL = 'https://restcountries.com/v3.1';
   return fetch(
     `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
